e951038f9bcc303c7f01e096a36aa814f400d7bd
Forian Fell <florian.fell@web-computing.de>
2022-11-25 11:48:12 +0100

    headless false

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,8 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-    video: 'on'
+    video: 'on',
+    headless: false
   },
 
   /* Configure projects for major browsers */
